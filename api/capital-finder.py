1aa97b3aa990914fa189fe1c3c104a385d09f8e8
Sara-Toughoj <ss.toughoj@gmail.com>
2023-06-15 22:30:01 +0300

    lab 16

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -2,45 +2,61 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
 
-class handler(BaseHTTPRequestHandler):
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    url_path = self.path
-    url_components = parse.urlsplit(url_path)
-    query_list = parse.parse_qsl(url_components.query)
-    my_dict = dict(query_list)
-    # print(111,my_dict)
-    if 'country' in my_dict:
-      contry = my_dict.get('country')
-      url= 'https://restcountries.com/v3.1/name/'
-      res = requests.get(url+contry)
-      data = res.json()
-      # print(222,data)
-      for country_data in data :
-        capital = country_data['capital'][0]
-        message = f'the capital of {contry} is {capital}'
-    elif 'capital' in my_dict:
-      cpital = my_dict.get('capital')
-      url= 'https://restcountries.com/v3.1/capital/'
-      res = requests.get(url+cpital)
-      data2 = res.json()
-      print(222,data2)
-      for country_data2 in data2 :
-        capital = country_data2['name']["common"]
-        message = f' {cpital} is the capital of  {capital}'
-    self.wfile.write(message.encode())
-    return
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        
+        url_path = self.path
+        url_components = parse.urlsplit(url_path)
+        query_list = parse.parse_qsl(url_components.query)
+        query_dict = dict(query_list)
+        
+        if 'country' in query_dict:
+            country_query = query_dict.get('country')
+            message = self.get_country_info(country_query)
+        
+        elif 'capital' in query_dict:
+            capital_query = query_dict.get('capital')
+            message = self.get_capital_info(capital_query)
+        
+        else:
+            message = "Invalid query parameter"
+        
+        self.wfile.write(message.encode())
+        return
 
+    def get_country_info(self, country_query):
+        country_url = f'https://restcountries.com/v3.1/name/{country_query}'
+        response = requests.get(country_url)
+        
+        if response.status_code == 200:
+            country_data = response.json()
+            
+            if country_data:
+                for country_info in country_data:
+                    capital = country_info['capital'][0]
+                    message = f"The capital of {country_query} is {capital}"
+                return message
+            else:
+                return "Data not found"
+        
+        return "API request failed"
 
-
-
-
-
-
-
-
-
-
-
+    def get_capital_info(self, capital_query):
+        capital_url = f'https://restcountries.com/v3.1/capital/{capital_query}'
+        response = requests.get(capital_url)
+        
+        if response.status_code == 200:
+            capital_data = response.json()
+            
+            if capital_data:
+                for capital_info in capital_data:
+                    country_name = capital_info['name']['common']
+                    message = f"{capital_query} is the capital of {country_name}"
+                return message
+            else:
+                return "Data not found"
+        
+        return "API request failed"
